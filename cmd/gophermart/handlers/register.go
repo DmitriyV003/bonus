0de1913947fb87e37cf57681df018dbf263bc9e2
dmitriyV25 <dmitriy.vorobev@onlinesup.com>
2022-10-12 18:31:03 +0300

    feat: registration
    (cherry picked from commit 4feab424670bee41e58ac3d7942fb1f984e9e50a)

--- cmd/gophermart/handlers/register.go
+++ cmd/gophermart/handlers/register.go
@@ -1,12 +1,38 @@
 package handlers
 
 import (
-	"github.com/DmitriyV003/bonus/cmd/gophermart/config"
+	"encoding/json"
+	"github.com/DmitriyV003/bonus/cmd/gophermart/application_errors"
+	"github.com/DmitriyV003/bonus/cmd/gophermart/container"
+	"github.com/DmitriyV003/bonus/cmd/gophermart/requests"
+	"github.com/DmitriyV003/bonus/cmd/gophermart/services"
+	"github.com/go-playground/validator/v10"
 	"net/http"
 )
 
-func RegisterHandler(container *config.Container) http.HandlerFunc {
-	return func(writer http.ResponseWriter, request *http.Request) {
+func RegisterHandler(container *container.Container) http.HandlerFunc {
+	return func(res http.ResponseWriter, request *http.Request) {
+		var regRequest requests.RegistrationRequest
 
+		validate := validator.New()
+
+		if err := json.NewDecoder(request.Body).Decode(&regRequest); err != nil {
+			application_errors.SwitchError(&res, err)
+			return
+		}
+
+		if err := validate.Struct(&regRequest); err != nil {
+			application_errors.WriteHTTPError(&res, http.StatusBadRequest, err)
+			return
+		}
+
+		service := services.NewUserService(container)
+		err := service.Create(&regRequest)
+		if err != nil {
+			application_errors.SwitchError(&res, err)
+			return
+		}
+
+		res.WriteHeader(http.StatusOK)
 	}
 }
