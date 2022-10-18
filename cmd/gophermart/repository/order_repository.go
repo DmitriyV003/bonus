2eebad62f0d1d486f2c0d9e05f25e2c9dfda3e0c
dmitriyV25 <dmitriy.vorobev@onlinesup.com>
2022-10-18 12:12:10 +0300

    feat: order creation

--- cmd/gophermart/repository/order_repository.go
+++ cmd/gophermart/repository/order_repository.go
@@ -2,8 +2,10 @@ package repository
 
 import (
 	"context"
+	"errors"
 	"github.com/DmitriyV003/bonus/cmd/gophermart/application_errors"
 	"github.com/DmitriyV003/bonus/cmd/gophermart/models"
+	"github.com/jackc/pgx/v5"
 	"github.com/jackc/pgx/v5/pgxpool"
 )
 
@@ -12,11 +14,13 @@ type OrderRepository struct {
 }
 
 func NewOrderRepository(pool *pgxpool.Pool) *OrderRepository {
-	return &OrderRepository{db: pool}
+	return &OrderRepository{
+		db: pool,
+	}
 }
 
-func (users *OrderRepository) Create(ctx context.Context, order *models.Order) error {
-	sql := `INSERT INTO orders (number, status, amount, user_id, created_at) VALUES ($1, $2, $3, $4, $5)`
+func (orders *OrderRepository) Create(ctx context.Context, order *models.Order) (*models.Order, error) {
+	sql := `INSERT INTO orders (number, amount, user_id, created_at) VALUES ($1, $2, $3, $4) RETURNING id`
 
 	//dbUser, err := users.GetByLogin(ctx, user.Login)
 	//if err != nil && !errors.Is(err, application_errors.ErrNotFound) {
@@ -31,11 +35,93 @@ func (users *OrderRepository) Create(ctx context.Context, order *models.Order) e
 	//	return application_errors.ErrNotFound
 	//}
 
-	_, err := users.db.Exec(ctx, sql, order.Number, order.Status, order.Amount, order.User.Id, order.CreatedAt)
-
+	var id int64
+	err := orders.db.QueryRow(ctx, sql, order.Number, order.Amount, order.User.Id, order.CreatedAt).Scan(&id)
 	if err != nil {
-		return application_errors.ErrInternalServer
+		return nil, application_errors.ErrInternalServer
 	}
 
-	return nil
+	order, err = orders.GetByIdWithUser(ctx, id)
+	if err != nil {
+		return nil, err
+	}
+
+	return order, nil
+}
+
+func (orders *OrderRepository) GetByIdWithUser(ctx context.Context, id int64) (*models.Order, error) {
+	sql := `SELECT 
+       orders.id, 
+       orders.number,
+       orders.status,
+       orders.amount,
+       orders.created_at, 
+       orders.updated_at, 
+       u.id, 
+       u.login, 
+       u.balance,
+       u.created_at, 
+       u.updated_at FROM orders JOIN users u on u.id = orders.user_id WHERE orders.id = $1`
+	var user models.User
+	var order models.Order
+
+	row := orders.db.QueryRow(ctx, sql, id)
+	err := row.Scan(
+		&order.Id,
+		&order.Number,
+		&order.Status,
+		&order.Amount,
+		&order.CreatedAt,
+		&order.UpdatedAt,
+		&user.Id,
+		&user.Login,
+		&user.Balance,
+		&user.CreatedAt,
+		&user.UpdatedAt,
+	)
+	order.User = &user
+	if err != nil && !errors.Is(err, pgx.ErrNoRows) {
+		return nil, err
+	}
+
+	if errors.Is(err, pgx.ErrNoRows) {
+		return nil, application_errors.ErrNotFound
+	}
+
+	return &order, nil
+}
+
+func (orders *OrderRepository) GetByNumber(ctx context.Context, number string) (*models.Order, error) {
+	sql := `SELECT 
+       orders.id, 
+       orders.number,
+       orders.status,
+       orders.amount,
+       orders.user_id,
+       orders.created_at, 
+       orders.updated_at
+       FROM orders WHERE orders.number = $1`
+	var order models.Order
+	var user models.User
+
+	row := orders.db.QueryRow(ctx, sql, number)
+	err := row.Scan(
+		&order.Id,
+		&order.Number,
+		&order.Status,
+		&order.Amount,
+		&user.Id,
+		&order.CreatedAt,
+		&order.UpdatedAt,
+	)
+	if err != nil && !errors.Is(err, pgx.ErrNoRows) {
+		return nil, err
+	}
+	order.User = &user
+
+	if errors.Is(err, pgx.ErrNoRows) {
+		return nil, application_errors.ErrNotFound
+	}
+
+	return &order, nil
 }
