55052a47ef33db1b2209795ee4d4249e4a200c4b
dmitriyV25 <dmitriy.vorobev@onlinesup.com>
2022-10-24 18:02:01 +0300

    feat: order polling

--- cmd/gophermart/main.go
+++ cmd/gophermart/main.go
@@ -1,27 +1,33 @@
 package main
 
 import (
+	"context"
 	"github.com/DmitriyV003/bonus/internal/application"
+	"github.com/DmitriyV003/bonus/internal/clients"
 	"github.com/DmitriyV003/bonus/internal/config"
 	"github.com/DmitriyV003/bonus/internal/container"
+	"github.com/DmitriyV003/bonus/internal/services"
 	"github.com/rs/zerolog/log"
 	"net/http"
 )
 
 func main() {
-	applicationApp := application.App{
+	app := application.App{
 		Conf:      config.Config{},
 		Container: &container.Container{},
 	}
-	applicationApp.Config()
+	app.Config()
 
-	defer applicationApp.Close()
+	defer app.Close()
 
-	log.Info().Msgf("server is starting at %s", applicationApp.Conf.Address)
+	log.Info().Msgf("server is starting at %s", app.Conf.Address)
 	srv := &http.Server{
-		Addr:    applicationApp.Conf.Address,
-		Handler: applicationApp.CreateHandler(),
+		Addr:    app.Conf.Address,
+		Handler: app.CreateHandler(),
 	}
+
+	orderService := services.NewOrderService(app.Container, nil, clients.NewBonusClient(app.Conf.AccrualAddress))
+	go orderService.PollPendingOrders(context.Background())
 
 	if err := srv.ListenAndServe(); err != nil {
 		log.Panic().Err(err)
--- internal/services/order_service.go
+++ internal/services/order_service.go
@@ -9,7 +9,9 @@ import (
 	"github.com/DmitriyV003/bonus/internal/container"
 	"github.com/DmitriyV003/bonus/internal/models"
 	"github.com/DmitriyV003/bonus/internal/policy"
+	"github.com/rs/zerolog/log"
 	"strconv"
+	"time"
 )
 
 type OrderService struct {
@@ -61,29 +63,34 @@ func (myself *OrderService) Create(user *models.User, orderNumber string) (*mode
 		return nil, fmt.Errorf("unable to create order in db: %w", err)
 	}
 
-	_, err = myself.bonusClient.CreateOrder(orderNumber)
-	if err != nil {
-		return nil, fmt.Errorf("unable to create order in black box: %w", err)
-	}
+	//_, err = myself.bonusClient.CreateOrder(orderNumber)
+	//if err != nil {
+	//	return nil, fmt.Errorf("unable to create order in black box: %w", err)
+	//}
+	//
+	//orderDetails, err := myself.bonusClient.GetOrderDetails(orderNumber)
+	//if err != nil {
+	//	return nil, fmt.Errorf("unable to get order details: %w", err)
+	//}
+	//
+	//order.Status = orderDetails.Status
+	//order.Amount = int64(orderDetails.Amount * 10000)
+	//err = myself.container.Orders.UpdateById(context.Background(), order)
+	//if err != nil {
+	//	return nil, fmt.Errorf("unable to create order in db: %w", err)
+	//}
+	//
+	//if orderDetails.Amount > 0 {
+	//	paymentService := NewPaymentService(myself.container)
+	//	err = paymentService.CreateAccrualPayment(user, int64(orderDetails.Amount*10000), orderNumber)
+	//	if err != nil {
+	//		return nil, fmt.Errorf("unable to create payment: %w", err)
+	//	}
+	//}
 
-	orderDetails, err := myself.bonusClient.GetOrderDetails(orderNumber)
+	err = myself.sendAndUpdateOrder(context.Background(), user, order)
 	if err != nil {
-		return nil, fmt.Errorf("unable to get order details: %w", err)
-	}
-
-	order.Status = orderDetails.Status
-	order.Amount = int64(orderDetails.Amount * 10000)
-	err = myself.container.Orders.UpdateById(context.Background(), order)
-	if err != nil {
-		return nil, fmt.Errorf("unable to create order in db: %w", err)
-	}
-
-	if orderDetails.Amount > 0 {
-		paymentService := NewPaymentService(myself.container)
-		err = paymentService.CreateAccrualPayment(user, int64(orderDetails.Amount*10000), orderNumber)
-		if err != nil {
-			return nil, fmt.Errorf("unable to create payment: %w", err)
-		}
+		return nil, fmt.Errorf("unable to update order: %w", err)
 	}
 
 	return order, nil
@@ -96,4 +103,78 @@ func (myself *OrderService) OrdersByUser(user *models.User) ([]*models.Order, er
 	}
 
 	return orders, nil
+}
+
+func (myself *OrderService) PollPendingOrders(ctx context.Context) {
+	ticker := time.NewTicker(1 * time.Second)
+	ctx, cancel := context.WithCancel(ctx)
+	defer ticker.Stop()
+
+	for {
+		select {
+		case <-ctx.Done():
+			cancel()
+			return
+		case <-ticker.C:
+			orders, err := myself.container.Orders.AllPending(ctx)
+			if err != nil {
+				cancel()
+				return
+			}
+
+			for _, order := range orders {
+				log.Info().Fields(map[string]interface{}{
+					"order_id": order.Id,
+					"status":   order.Status,
+					"number":   order.Number,
+				}).Msg("Polling order")
+
+				user, err := myself.container.Users.GetById(ctx, order.User.Id)
+				if err != nil && !errors.Is(err, application_errors.ErrNotFound) {
+					cancel()
+					return
+				}
+
+				err = myself.sendAndUpdateOrder(ctx, user, order)
+				if err != nil && !errors.Is(err, application_errors.ErrServiceUnavailable) {
+					log.Error().Err(err)
+					cancel()
+				}
+			}
+		}
+	}
+}
+
+func (myself *OrderService) sendAndUpdateOrder(ctx context.Context, user *models.User, order *models.Order) error {
+	//_, err := myself.bonusClient.CreateOrder(order.Number)
+	//if err != nil {
+	//	log.Error().Err(err)
+	//	return fmt.Errorf("unable to create order in black box: %w", err)
+	//}
+
+	orderDetails, err := myself.bonusClient.GetOrderDetails(order.Number)
+	if err != nil {
+		log.Error().Err(err)
+		return fmt.Errorf("unable to get order details: %w", err)
+	}
+
+	if orderDetails != nil && order.Status != orderDetails.Status {
+		order.Status = orderDetails.Status
+		order.Amount = int64(orderDetails.Amount * 10000)
+		err = myself.container.Orders.UpdateById(context.Background(), order)
+		if err != nil {
+			log.Error().Err(err)
+			return fmt.Errorf("unable to create order in db: %w", err)
+		}
+
+		if orderDetails.Amount > 0 {
+			paymentService := NewPaymentService(myself.container)
+			err = paymentService.CreateAccrualPayment(user, int64(orderDetails.Amount*10000), order.Number)
+			if err != nil {
+				return fmt.Errorf("unable to create payment: %w", err)
+			}
+		}
+	}
+
+	return nil
 }
